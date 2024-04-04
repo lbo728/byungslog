8a396677177d89440bcaa35963fe59b1ce8584b5
lbo728 <72309817+lbo728@users.noreply.github.com>
2024-04-04 23:59:08 +0900

    design: Footer & BaseLayout

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,5 +4,5 @@ import preact from "@astrojs/preact";
 // https://astro.build/config
 export default defineConfig({
   site: "https://byungsker.netlify.app/",
-  integrations: [preact()]
+  integrations: [preact()],
 });
