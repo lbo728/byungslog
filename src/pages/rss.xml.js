3319848d5b57cadb096713f76044de7f570fb87b
lbo728 <72309817+lbo728@users.noreply.github.com>
2024-04-05 00:44:10 +0900

    fix: rss 파일

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -1,13 +1,13 @@
-import rss from '@astrojs/rss';
-import { getCollection } from 'astro:content';
+import rss from "@astrojs/rss";
+import { getCollection } from "astro:content";
 
 export async function GET(context) {
-  const posts = await getCollection('post');
+  const posts = await getCollection("post");
   return rss({
-    title: 'Astro Learner | Blog',
-    description: 'My journey learning Astro',
+    title: "Byungsker | Blog",
+    description: "Tech, Design, Book, Mindset, Writing ",
     site: context.site,
-    items: posts.map(post => ({
+    items: posts.map((post) => ({
       title: post.data.title,
       pubDate: post.data.pubData,
       description: post.data.description,
