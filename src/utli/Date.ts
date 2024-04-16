8c1a4a6fb0f9132a12426cc98c341e2c538993f2
bwlee-dix <72309817+lbo728@users.noreply.github.com>
2024-04-17 07:58:56 +0900

    fix: 빌드 이슈 해결

--- src/utli/Date.ts
+++ src/utli/Date.ts
@@ -4,9 +4,6 @@ export function formatPubDate(pubDate: string): string {
   var year = date.getFullYear();
   var month = date.getMonth() + 1;
   var day = date.getDate();
-  // var dayNames = ["일", "월", "화", "수", "목", "금", "토"];
-  // var dayIndex = date.getDay();
-  // var dayName = dayNames[dayIndex];
 
-  return year + "년 " + month + "월 " + day + "일";
+  return year + '년 ' + month + '월 ' + day + '일';
 }
