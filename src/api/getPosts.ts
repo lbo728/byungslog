e1c5fa389128f8319f98d251e29b6ebeb1a1675f
lbo728 <72309817+lbo728@users.noreply.github.com>
2024-09-01 22:07:45 +0900

    style: adjust styling on blog page

--- src/api/getPosts.ts
+++ src/api/getPosts.ts
@@ -3,7 +3,7 @@ import { supabase } from "../lib/supabase";
 export async function getPosts() {
   const { data, error } = await supabase
     .from("posts")
-    .select("title,id,tags,slug")
+    .select("title,id,tags,slug,created_at")
     .order("created_at", { ascending: false });
 
   if (error) {
--- src/utils/date.ts
+++ src/utils/date.ts
@@ -7,3 +7,16 @@ export function formatPubDate(pubDate: string): string {
 
   return year + "년 " + month + "월 " + day + "일";
 }
+
+export function formatDate(isoString: string) {
+  const date = new Date(isoString);
+
+  const year = date.getFullYear();
+  const month = date.getMonth() + 1;
+  const day = date.getDate();
+  const hours = date.getHours();
+  // const minutes = date.getMinutes();
+
+  return `${year}년 ${month}월 ${day}일 ${hours}시 `;
+  // return `${year}년 ${month}월 ${day}일 ${hours}시 ${minutes}분`;
+}
