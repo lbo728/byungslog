917c5badd7ca43768495eb2a752556f05f8fe403
lbo728 <72309817+lbo728@users.noreply.github.com>
2024-09-05 22:47:06 +0900

    feat: add units to 'Date' utility functions

--- src/utils/date.ts
+++ src/utils/date.ts
@@ -15,8 +15,8 @@ export function formatDate(isoString: string) {
   const month = date.getMonth() + 1;
   const day = date.getDate();
   const hours = date.getHours();
-  // const minutes = date.getMinutes();
+  const minutes = date.getMinutes();
+  const seconds = date.getSeconds();
 
-  return `${year}년 ${month}월 ${day}일 ${hours}시 `;
-  // return `${year}년 ${month}월 ${day}일 ${hours}시 ${minutes}분`;
+  return `${year}년 ${month}월 ${day}일 ${hours}시 ${minutes}분 ${seconds}초 `;
 }
