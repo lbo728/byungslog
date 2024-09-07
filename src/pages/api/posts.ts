ef2d46d19d8c442f1232b59bcefdb28d56b306a4
lbo728 <extreme93@naver.com>
2024-09-07 23:58:02 +0900

    fix: cache adjust in api posts

--- src/pages/api/posts.ts
+++ src/pages/api/posts.ts
@@ -1,16 +1,17 @@
-import { createHash } from "crypto";
+// import { createHash } from "crypto";
 import getPosts from "../../service/getPosts";
 import type { Post } from "../../types/Post";
 
 export async function GET() {
   const posts: Post[] = await getPosts();
-  const etag = createHash("md5").update(JSON.stringify(posts)).digest("hex");
+  // const etag = createHash("md5").update(JSON.stringify(posts)).digest("hex");
 
   return new Response(JSON.stringify({ posts }), {
     headers: {
       "Content-Type": "application/json",
-      "Cache-Control": "public, max-age=0", // 캐시 시간 설정
-      ETag: etag,
+      // "Cache-Control": "public, max-age=0", // 캐시 시간 설정
+      // ETag: etag,
+      "Cache-Control": "no-cache, no-store, must-revalidate", // 캐시 무효화
     },
   });
 }
