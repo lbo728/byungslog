34469b15b902b901e5dc905fe7235b1ee3194b63
lbo728 <extreme93@naver.com>
2024-09-07 23:53:04 +0900

    fix: cache adjust in api posts

--- src/pages/api/posts.ts
+++ src/pages/api/posts.ts
@@ -1,9 +1,26 @@
+import { createHash } from "crypto";
 import getPosts from "../../service/getPosts";
-import type { Post } from "../../types/Post";
+import type { Post } from "src/types/Post";
 
 export async function GET() {
   const posts: Post[] = await getPosts();
+  const etag = createHash("md5").update(JSON.stringify(posts)).digest("hex");
+
   return new Response(JSON.stringify({ posts }), {
-    headers: { "Content-Type": "application/json" },
+    headers: {
+      "Content-Type": "application/json",
+      "Cache-Control": "public, max-age=0", // 캐시 시간 설정
+      ETag: etag,
+    },
   });
 }
+
+// import getPosts from "../../service/getPosts";
+// import type { Post } from "../../types/Post";
+
+// export async function GET() {
+//   const posts: Post[] = await getPosts();
+//   return new Response(JSON.stringify({ posts }), {
+//     headers: { "Content-Type": "application/json" },
+//   });
+// }
