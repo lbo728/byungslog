d7d04483275c54379c4f4af1a464779c1550f3b8
lbo728 <extreme93@naver.com>
2024-09-18 14:55:20 +0900

    feat: add highlight and marked-highlight to marked

--- src/utils/marked.ts
+++ src/utils/marked.ts
@@ -1,6 +1,17 @@
 import { Marked } from "marked";
+import { markedHighlight } from "marked-highlight";
+import hljs from "highlight.js";
+import "highlight.js/styles/github-dark-dimmed.css";
 
-const marked = new Marked();
+const marked = new Marked(
+  markedHighlight({
+    langPrefix: "hljs language-",
+    highlight(code, lang, info) {
+      const language = hljs.getLanguage(lang) ? lang : "plaintext";
+      return hljs.highlight(code, { language }).value;
+    },
+  }),
+);
 
 export async function markedWriteContent(
   inputContent: string,
