f1cdc53422089ec5385de8b830c91a3a050bbe31
lbo728 <72309817+lbo728@users.noreply.github.com>
2024-10-28 21:24:58 +0900

    WIP: ThumbnailModal in admin

--- src/pages/api/posts.ts
+++ src/pages/api/posts.ts
@@ -1,27 +1,13 @@
-// import { createHash } from "crypto";
 import getPosts from "../../service/getPosts";
 import type { Post } from "../../types/Post";
 
 export async function GET() {
   const posts: Post[] = await getPosts();
-  // const etag = createHash("md5").update(JSON.stringify(posts)).digest("hex");
 
   return new Response(JSON.stringify({ posts }), {
     headers: {
       "Content-Type": "application/json",
-      // "Cache-Control": "public, max-age=0", // 캐시 시간 설정
-      // ETag: etag,
-      "Cache-Control": "no-cache, no-store, must-revalidate", // 캐시 무효화
+      "Cache-Control": "no-cache, no-store, must-revalidate",
     },
   });
 }
-
-// import getPosts from "../../service/getPosts";
-// import type { Post } from "../../types/Post";
-
-// export async function GET() {
-//   const posts: Post[] = await getPosts();
-//   return new Response(JSON.stringify({ posts }), {
-//     headers: { "Content-Type": "application/json" },
-//   });
-// }
