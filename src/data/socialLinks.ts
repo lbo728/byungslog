0e05b584acceb28c00b24349b3dfa847435b73b1
lbo728 <72309817+lbo728@users.noreply.github.com>
2025-01-09 23:36:18 +0900

    refactor: reuse link and icon components in about and footer

---
+++ src/data/socialLinks.ts
@@ -0,0 +1,8 @@
+const socialLinks = {
+  linkedin: "https://www.linkedin.com/in/byungwoo-lee-98a253200/",
+  github: "https://github.com/lbo728",
+  threads: "https://www.threads.net/@byungsker_letter",
+  x: "https://twitter.com/byungsker",
+};
+
+export default socialLinks;
